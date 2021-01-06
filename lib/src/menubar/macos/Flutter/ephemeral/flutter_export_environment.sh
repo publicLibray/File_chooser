32db0ede0d3458b3451cf7a1fd4f8ebe67419223
mengsieng <mengsieng.him@trendsoftinnovation.com>
2021-01-06 14:12:35 +0700

    commit

---
+++ lib/src/menubar/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/trendsoft/desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/trendsoft/Downloads/file_chooser-file_chooser_fde/plugins/menubar"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/trendsoft/desktop/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
